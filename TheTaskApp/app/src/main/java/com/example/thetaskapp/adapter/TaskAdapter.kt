4d7407eb9ea2321fb79ac973c414287575a49add
DilekaTharuki <dilekagunawardana21@gmail.com>
2024-05-12 16:40:34 +0530

    Task-App

---
+++ TheTaskApp/app/src/main/java/com/example/thetaskapp/adapter/TaskAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.thetaskapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.thetaskapp.databinding.TaskLayoutBinding
+import com.example.thetaskapp.fragments.HomeFragmentDirections
+import com.example.thetaskapp.model.Task
+
+class TaskAdapter :RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    class TaskViewHolder(val itemBinding: TaskLayoutBinding):RecyclerView.ViewHolder(itemBinding.root)
+
+    private val differentCallback = object :DiffUtil.ItemCallback<Task>(){
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.taskDesc ==newItem.taskDesc &&
+                    oldItem.taskTitle == newItem.taskTitle&&
+                    oldItem.priority == newItem.priority &&
+                    oldItem.date == newItem.date
+
+
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    val differ = AsyncListDiffer(this,differentCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+       return TaskViewHolder(
+           TaskLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+       )
+    }
+
+    override fun getItemCount(): Int {
+       return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+       val currentTask = differ.currentList[position]
+        holder.itemBinding.noteTitle.text = currentTask.taskTitle
+        holder.itemBinding.noteDesc.text = currentTask.taskDesc
+        holder.itemBinding.priority.text = currentTask.priority
+        holder.itemBinding.date.text = currentTask.date
+
+
+
+        holder.itemView.setOnClickListener{
+            val direction = HomeFragmentDirections.actionHomeFragmentToEditTaskFragment(currentTask)
+            it.findNavController().navigate(direction)
+        }
+
+
+    }
+}
